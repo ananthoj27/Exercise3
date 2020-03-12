fd4e4eb8473a7dc5b5af35a8643682434a342782
Shashank Ananthoju <ananthoj@sheridancollege.ca>
2020-03-12 11:03:44 -0400

    made 1 dice work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   header = 'Exercise 3';
+  image1: string;
+  alt1: string;
+
+  constructor() {
+    this.image1 = 'assets/images/dice/side1.png';
+    this.alt1 = 'side 1';
+  }
+  roll(){
+    const rand1 = 1 + Math.floor( 6 * Math.random());
+    this.image1 = `assets/images/dice/side${rand1}.png`;
+    this.alt1 = `side ${rand1}`;
+  }
+
 }
